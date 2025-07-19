53fb74a4159901394daf2079aaad313906aa770e
Anurag Ramdasan <anurag.ramdasan@gmail.com>
2025-07-19 12:55:51 +0530

    fix play button sizing

--- src/components/dashboard/AudioPlayer.tsx
+++ src/components/dashboard/AudioPlayer.tsx
@@ -72,11 +72,11 @@ export const AudioPlayer: React.FC<AudioPlayerProps> = ({
               )}
             >
               {isCurrentTrack && isPlaying ? (
-                <svg className="w-8 h-8" fill="currentColor" viewBox="0 0 20 20">
+                <svg className="w-12 h-12" fill="currentColor" viewBox="0 0 20 20">
                   <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zM7 8a1 1 0 012 0v4a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v4a1 1 0 102 0V8a1 1 0 00-1-1z" clipRule="evenodd" />
                 </svg>
               ) : (
-                <svg className="w-8 h-8 ml-0.5" fill="currentColor" viewBox="0 0 20 20">
+                <svg className="w-12 h-12 ml-0.5" fill="currentColor" viewBox="0 0 20 20">
                   <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM9.555 7.168A1 1 0 008 8v4a1 1 0 001.555.832l3-2a1 1 0 000-1.664l-3-2z" clipRule="evenodd" />
                 </svg>
               )}
