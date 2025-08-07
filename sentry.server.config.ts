46242ce7f950b1975f1c5b756ab57c4393c32b37
Anurag Ramdasan <anurag.ramdasan@gmail.com>
2025-08-07 11:20:58 +0530

    setup sentry for biirbal

---
+++ sentry.server.config.ts
@@ -0,0 +1,18 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://a123977cb8ae7960a4a56cd7366504f2@o4509801215229952.ingest.us.sentry.io/4509801219489792",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
